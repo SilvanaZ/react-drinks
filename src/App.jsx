5cc29988392104be238d1ffa1f007cf1af32c6d7
Silvana Zalazar <90283693+SilvanaZ@users.noreply.github.com>
2023-07-14 18:27:40 -0300

    Agregando funcionalidad al modal cart

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,22 @@ import { DrinksProvider } from "./context/DrinksProvider"
 import { CartProvider } from "./context/CartProvider"
 import MainLayout from "./layout"
 import AppRoutes from "./routes"
+import { ModalProvider } from "./context/ModalProvider"
 
 function App() {
 
   return (
+    <ModalProvider>
+      <CartProvider>
     <MainLayout>
       <DrinksProvider>
       <CategoriesProvider>
-        <CartProvider>
           <AppRoutes />
-        </CartProvider>
       </CategoriesProvider>
       </DrinksProvider>
     </MainLayout>
+    </CartProvider>
+    </ModalProvider>
   )
 }
 
--- src/components/CartModal/index.jsx
+++ src/components/CartModal/index.jsx
@@ -3,39 +3,54 @@ import styles from "./CartModal.module.css"
 import { faXmarkCircle } from "@fortawesome/free-regular-svg-icons";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { useModalCart } from "../../hooks/useModalCart";
+import { useCart } from "../../hooks/useCart";
 
 function CartModal () {
-    const { isOpen, toogleModal } = useModalCart()
+    const { isOpen, toogleModal } = useModalCart();
+    const {cart, 
+        addToCart, 
+        removeOneFromCart, 
+        removeAllFromCart, 
+        clearCart } = useCart();
+    
+        if (isOpen)
     return (
-        <div className={styles.modalBg}>
+        <div className={styles.modalBg} onClick={toogleModal}>
             <div className={styles.modal}>
                 <FontAwesomeIcon icon={faXmarkCircle} className={styles.icon}/>
                 <h2>Mi carrito</h2>
                 <section className={styles.modalBody}>
                     <div className={styles.modalDrinkListContainer}>
-                        <article className={styles.card}>
-                            <img src="https://www.thecocktaildb.com/images/media/drink/tqxyxx1472719737.jpg" alt="" className={styles.img} />
-                            <span>Nombre</span>
-                            <span>Precio</span>
-                            <div className={styles.counter}>
-                                <button>
-                                    -
-                                </button>
-                                <span>
-                                    2
-                                </span>
-                                <button>
-                                    +
-                                </button>
-                            </div>
-                            <FontAwesomeIcon icon={faTrash} className={styles.iconTrash}/>
-                        </article>
+                    {cart.map((drink)=>(
+                        <article key={drink.idDrink} className={styles.card}>
+                        <img src={drink.strDrinkThumb} alt=""/>
+                        <span>Nombre</span>
+                        <span>Precio</span>
+                        <div className={styles.counter}>
+                            <button onClick={()=> removeOneFromCart}>
+                                -
+                            </button>
+                            <span>
+                                {drink.quantity}
+                            </span>
+                            <button onClick={()=> addToCart(drink)}>
+                                +
+                            </button>
+                        </div>
+                        <FontAwesomeIcon 
+                        icon={faTrash} 
+                        className={styles.iconTrash}
+                        onClick={()=> removeAllFromCart}/>
+                    </article>
+))
+}
+                        
                     </div>
                     <aside>
                         <p>subtotal: $XXXXXX</p>
                         <p>total: $XXXXXX</p>
                         <div className={styles.btnContainer}>
-                            <button className={styles.clearCart}>Vaciar Carrito</button>
+                            <button className={styles.clearCart} onClick={clearCart}>Vaciar Carrito</button>
                             <button className={styles.confirmCart}>Confirmar Compra</button>
                         </div>
                     </aside>
