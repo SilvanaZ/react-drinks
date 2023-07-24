1d3a70b1961cf5d618743f5d74db103cc4db66e6
Silvana Zalazar <90283693+SilvanaZ@users.noreply.github.com>
2023-07-23 23:07:00 -0300

    Arreglando Copyright

--- src/pages/user/copyright/index.jsx
+++ src/pages/user/copyright/index.jsx
@@ -5,9 +5,9 @@ import { Link } from "react-router-dom";
 export default function Copyright(props) {
     return (
         <Typography variant="body2" color="text.secondary" align="center" {...props}>
-            {'Copyright © '}
-            <Link color="inherit" to="https://mui.com/">
-                React Drinks
+            {'Copyright © to React Drinks by'}
+            <Link color="inherit" to="https://github.com/silvanaZ/react-drinks/">
+                Silvana Zalazar ✨
             </Link>{' '}
             {new Date().getFullYear()}
             {'.'}
